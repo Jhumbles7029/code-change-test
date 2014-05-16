d2220e6ad952327b7e99ab54d140fc21a32c37a8
John Humbles <jhumbles7029@sohamcollege.org.uk>
2014-05-16 11:00:46 +0100

    Update greenBottles.py

--- greenBottles.py
+++ greenBottles.py
@@ -1,4 +1,19 @@
-print("10 green bottles, hanging on the wall")
-print("10 green bottles, hanging on the wall")
-print("And if 1 green bottle should acidentally fall,")
-print("They'll be 9 green bottles hanging on the wall.")
+import time
+bottles = 10
+while bottles > 0:
+  print("{0} green bottles, hanging on the wall".format(bottles))
+  time.sleep(2.5)
+  print("{0} green bottles, hanging on the wall".format(bottles))
+  time.sleep(2.5)
+  print("And if 1 green bottle should acidentally fall,")
+  time.sleep(2.5)
+  bottles = bottles - 1
+  print("They'll be {0} green bottles hanging on the wall.\n".format(bottles))
+  time.sleep(3.5)
+print("No green bottles, hanging on the wall")
+time.sleep(2.5)
+print("No green bottles, hanging on the wall")
+time.sleep(2.5)
+print("And I don't know how to sing this part of the song, so:")
+time.sleep(2.5)
+print("There's no green bottles hanging on the wall.")
